16054353a6494ca8795e2b189c37cb3aea7db2c8
microsizer201a <dimanalter@rambler.ru>
2024-08-20 18:37:25 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first = int(input())
+second = int(input())
+third =int(input())
+if first == second and second == third and third == first:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else: print(0)
